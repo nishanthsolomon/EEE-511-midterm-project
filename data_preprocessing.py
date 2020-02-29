bd350a2b7f54adddea6c12482e064d83f2c6c57e
sushilkumarmk <61368283+sushilkumarmk@users.noreply.github.com>
2020-02-29 10:21:53 -0700

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -25,13 +25,10 @@ x = combined_csv[['gdp','life_expectancy','freedom','generosity','corruption']]
 #normalized_X = preprocessing.normalize(x)
 #normalized_Y = preprocessing.normalize(y)
 
-x_train, x_test_valid, y_train, y_test_valid = train_test_split(x, y, test_size=0.20, random_state=42)
-x_test, x_valid, y_test, y_valid = train_test_split(x_test_valid, y_test_valid, test_size=0.5, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
 train = pd.concat([y_train, x_train], axis=1, sort=False)
 test = pd.concat([y_test, x_test], axis=1, sort=False)
-valid = pd.concat([y_valid, x_valid], axis=1, sort=False)
 
 train.to_csv(os.path.splitext("./dataset/train")[0]+ '.csv',index=False,sep=",")
 test.to_csv(os.path.splitext("./dataset/test")[0]+ '.csv',index=False,sep=",")
-valid.to_csv(os.path.splitext("./dataset/valid")[0]+ '.csv',index=False,sep=",")
